d7a373dc26b04b6303f58c9e503d1d80b27f4694
survibo <survibo@gmail.com>
2026-05-06 08:09:27 +0900

    nari

--- src/stories/GroupPage.stories.tsx
+++ src/stories/GroupPage.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { expect, userEvent, waitFor, within } from 'storybook/test';
 import { GroupPage } from '../components/pages';
 
 const meta = {
@@ -13,18 +12,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-
-    await expect(canvas.getAllByText('nari')[0]).toBeInTheDocument();
-    await expect(canvas.getAllByText('jun')[0]).toBeInTheDocument();
-    await expect(canvas.getAllByText('seo')[0]).toBeInTheDocument();
-    await expect(canvas.queryByText('mina')).not.toBeInTheDocument();
-
-    await userEvent.click(canvas.getByRole('button', { name: /more stories/i }));
-    await waitFor(() => {
-      expect(canvas.getAllByText('mina')[0]).toBeInTheDocument();
-    });
-  },
-};
+export const Default: Story = {};
